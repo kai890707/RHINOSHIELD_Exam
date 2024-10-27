4133e03b27e28a8b96477423a150298077d8f934
蔡銘凱 <z85385637@gmail.com>
2024-10-27 14:41:03 +0800

    Modified: Separate the Service logic and modify the docker yaml and Readme.

---
+++ app/public/sentenceService.php
@@ -0,0 +1,43 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+/**
+ * @author [Kai]
+ * @email [z85385637@gmail.com]
+ * @create date 2024-10-19 13:33:15
+ * @modify date 2024-10-19 13:33:15
+ * @desc Metaphorsum進入點
+ */
+use App\Service\Exam_one\SentenceService;
+use App\Service\Exam_one\MetaphorsumApiClient;
+use App\Service\Exam_one\ItsthisforthatApiClient;
+
+// 考量到終端與網頁顯示，因此全用\n進行換行
+
+#-----------------------題目1進階-使用抽換依賴類別實現題目需求-----------------------------
+/**
+ * MetaphorsumApiClient 使用範例
+ */
+$metaphorsumApiClient = new MetaphorsumApiClient();
+$metaphorsumservice = new SentenceService($metaphorsumApiClient);
+
+if (PHP_SAPI === 'cli') {
+    echo "metaphorsum result : " . $metaphorsumservice->getSentence() . PHP_EOL;
+} else {
+    echo "metaphorsum result : " . $metaphorsumservice->getSentence() . "<br>";
+}
+
+
+/**
+ * ItsthisforthatApiClient 使用範例
+ */
+$itsthisforthatApiClient = new ItsthisforthatApiClient();
+$itsthisforthatservice = new SentenceService($itsthisforthatApiClient);
+
+if (PHP_SAPI === 'cli') {
+    echo "itsthisforthat result : " . $itsthisforthatservice->getSentence() . PHP_EOL;
+} else {
+    echo "itsthisforthat result : " . $itsthisforthatservice->getSentence() . "<br>";
+}
+
+
